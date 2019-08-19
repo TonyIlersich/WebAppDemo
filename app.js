0b9a2d61f67d277631a8a3d31aefc57321fc2a90
TonyItchystich <antonio.ilersich@gmail.com>
2019-08-18 20:23:08 -0400

    setup canvas, game loop, and fps tracker

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const bodyParser = require('body-parser');
 startServer();
 
 function startServer() {
-	console.log('starting');
+	console.log('server starting');
 	
 	const app = express();
 	
 	app.set('view engine', 'ejs');
-	app.set('views', __dirname + '/views');
+	app.set('views', `${__dirname}/src`);
 	app.use(bodyParser.urlencoded());
 
 	app.get('/', (req, res) => res.render('index'));
 	app.get('/game.js', (req, res) => {
-		res.status(200).sendFile(`${__dirname}/views/game.js`);
+		res.status(200).sendFile(`${__dirname}/src/game.js`);
 	});
 	
 	const port = 8081;
@@ -24,5 +24,5 @@ function startServer() {
 		port,
 		() => console.log(`Server running on port ${port}`));
 	
-	console.log('started');
+	console.log('server start complete');
 }
